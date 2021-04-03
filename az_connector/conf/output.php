189635da274828b4f3f3ebc3962aed00bf861636
g.caglio@cantylab.net <demouser@SRFBOOK2.localdomain>
2021-04-03 09:49:30 +0200

    Added azure conf files

---
+++ az_connector/conf/output.php
@@ -0,0 +1,17 @@
+<?php
+$output_path="/home/demouser/cmdbcloudconnector/az_connector/output";
+
+// output file anag
+$out_vm_filename="vm.csv";
+$out_disks_filename="disks.csv";
+$out_subs_filename="subs.csv";
+
+// output file relations
+$out_rel_disks_vm="rel_disks-vm.csv";
+$out_rel_subs_disk="rel_subs-disk.csv";
+$out_rel_subs_vm="rel_subs-vm.csv";
+$out_rel_busapplandscape_vm="rel_bal-vm.csv";
+$out_rel_busapp_busapplandscape="rel_ba-bal.csv";
+
+
+?>
---
+++ az_connector/conf/tags.php
@@ -0,0 +1,6 @@
+<?php
+$tag_appid="AppID";
+$tag_appid_separator="|";
+$tag_landscape="Landscape";
+
+?>
